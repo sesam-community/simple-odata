92f263bdba41986b12d494e71d85f9406b7d4dfe
Tarjei Skrede <tarjeiskrede@M-C02Y7599JGH6.local>
2021-04-13 16:54:00 +0200

    Added tmp functionality to choose whether to stream data or not

--- service/odata-simple.py
+++ service/odata-simple.py
@@ -11,6 +11,7 @@ logger = logger.Logger('odata-simple')
 url = os.environ.get("base_url")
 value_field = os.environ.get("value_field", "value")
 log_response_data = os.environ.get("log_response_data", "false").lower() == "true"
+stream_data = os.environ.get("stream_data", "true").lower() == "true"
 
 
 def stream_odata_json(odata):
@@ -37,14 +38,17 @@ def get(path):
     logger.info("Request url: %s", request_url)
 
     try:
-        request_data = requests.get(request_url).text
+        request_data = requests.get(request_url)
         if log_response_data:
-            logger.info("Data received: %s", request_data)
+            logger.info("Data received: %s", request_data.text)
     except Exception as e:
         logger.warning("Exception occurred when download data from '%s': '%s'", request_url, e)
         raise
 
-    return Response(stream_odata_json(request_data), mimetype='application/json')
+    if stream_data:
+        return Response(stream_odata_json(request_data.text), mimetype='application/json')
+
+    return Response(request_data, mimetype='application/json')
 
 
 if __name__ == '__main__':
