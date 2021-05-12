59dcba6a7a761ec0c0421d16dbb201f8410a6d02
Tarjei Skrede <tarjeiskrede@M-C02Y7599JGH6.local>
2021-05-12 16:01:09 +0200

    Added debug_starting_offset env.var (not visible in readme)

--- service/odata-simple.py
+++ service/odata-simple.py
@@ -12,10 +12,10 @@ logger = logger.Logger('odata-simple')
 
 url = os.environ.get("base_url")
 value_field = os.environ.get("value_field", "value")
-page_size = os.environ.get("page_size", 1000)
+page_size = int(os.environ.get("page_size", 1000))
 use_count = os.environ.get("use_count_for_paging", "false").lower() == "true"
 headers = ujson.loads('{"Content-Type": "application/json"}')
-starting_offset = os.environ.get("debug_starting_offset", 0)
+starting_offset = int(os.environ.get("debug_starting_offset", 0))
 
 class BasicUrlSystem:
     def __init__(self, config):
@@ -45,7 +45,7 @@ class DataAccess:
         else:
             next_page = "{0}?$top={1}{2}".format(request_url, page_size, use_count_parameter)
 
-        entity_count = 26000
+        entity_count = starting_offset
         page_count = 0
         count = None
         previous_page = None
