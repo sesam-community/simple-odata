c86c041a8988c469e9d65d8078f64299885512e3
Tarjei Skrede <tarjeiskrede@M-C02Y7599JGH6.local>
2021-08-04 10:45:20 +0200

    Added more log lines for debugging

--- service/odata-simple.py
+++ service/odata-simple.py
@@ -108,7 +108,9 @@ class DataAccess:
             logger.error(error_text)
             raise AssertionError(error_text)
 
+        logger.info("Read request data and parse as dict!")
         result_json = Dotdictify(request_data.json())
+        logger.info("Extract entities from dict!")
         entities = result_json.get(key)
         request_data = None
         result_json = None
